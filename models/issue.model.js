0e4ad91ac8b94070f037c9a8040cc49dd2889a40
BrianLim80 <brian.lim.kc@gmail.com>
2021-06-18 17:15:26 +0800

    completed models

--- models/issue.model.js
+++ models/issue.model.js
@@ -2,11 +2,31 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const issuesSchema = new Schema({
-    name: String,
-    parentFolder: {
+    issueID : Number,
+    picture : String,
+    description: String,
+    date: String,
+    time: String,
+    location: String,
+    rating: Number,
+    updates: [{
         type: Schema.Types.ObjectId,
-        ref: "Folder"
-    }
+        ref: "Update"
+    }],
+    issueStatus: {
+        type: String,
+        required: true,
+        default: "Open",
+        enum: ["Open","In Progress","Closed","Deleted"]
+    },
+    userID: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    staffID: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
 })
 
 module.exports = mongoose.model("Issue", issuesSchema)
--- models/voucher.model.js
+++ models/voucher.model.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const issuesSchema = new Schema({
-    name: String,
-    parentFolder: {
+const voucherSchema = new Schema({
+    picture: String,
+    description: String,
+    pointsCost: Number,
+    status: {
+        type: String,
+        required: true,
+        default: "Redeemed",
+        enum: ["Redeemed","Used"]
+        },
+    issuedTo: {
         type: Schema.Types.ObjectId,
-        ref: "Folder"
+        ref: "User"
     }
 })
 
-module.exports = mongoose.model("Issue", issuesSchema)
+module.exports = mongoose.model("Voucher", voucherSchema)
