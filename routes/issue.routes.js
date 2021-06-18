6a551d2c14b603e8a8e8336a555c3f414838b562
Boon <boonteck.soh@hotmail.com>
2021-06-18 19:16:52 +0800

    sketch of issue routes

--- routes/issue.routes.js
+++ routes/issue.routes.js
@@ -1,7 +1,66 @@
 const router = require('express').Router()
 require('dotenv').config()
+const IssueModel = require('../models/issue.model')
+const UserModel = require('../models/user.model')
+
+//for staff
+router.get('/', async (req, res) => {
+    try {
+        let globalArrayOfIssues = await IssueModel.find()
+            .populate("updates")
+            .populate("issueStatus")
+            .populate("userID")
+            .populate("staffID")
 
 
+        res.status(200).json({globalArrayOfIssues})
+    } catch (e)
+    {
+        console.log(e)
+        res.status(400).json({"message" : e})
+    }
+})
+
+//For individual user (UserId) Get their own issues array
+router.get('/:id', async(req, res) => {
+    try {
+        let user = await UserModel.find()
+            .populate("pendingIssues")
+            .populate("closedIssues")
+            .populate("voucherList")
+
+        //let individualIssues =
+
+        res.status(200).json({individualIssues})
+    } catch (e)
+    {
+        console.log(e)
+        res.status(400).json({"message" : e})
+    }
+})
+
+//Issue Form - Post request
+router.post('/add/:issueID', async (req, res) => {
+    const issues = new IssueModel(req.body)
+    try {
+        res.status(201).json({issues})
+    } catch(e){
+        console.log(e)
+        res.status(400).json
+    }
+})
+
+// //For editing issues status //only done layout
+// router.put("/edit", async(req, res)=> {
+//
+//     //splice or filter here
+//
+//     try {
+//         res.status(200).json({})
+//     }catch(e){
+//
+//     }
+// })
 
 
 module.exports = router
